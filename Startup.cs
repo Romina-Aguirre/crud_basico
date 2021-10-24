6802a5824074f98524b78b082085fd982999d9d3
lucho <lucianosantillan96@outlook.com.ar>
2021-10-24 15:00:32 -0300

    Agregar archivos de proyecto.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CRUD_Alumnos2.Startup))]
+namespace CRUD_Alumnos2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
